8217826e0894185974ecd73d7382f836567bdb18
DvK42 <joan.llan8s@gmail.com>
2022-12-18 14:40:26 +0100

    add calculate result functionality

--- script.js
+++ script.js
@@ -1,35 +1,90 @@
 function add(a, b) {
+  if (a.length === 0 || b.length === 0) {
+    return;
+  }
   return parseInt(a) + parseInt(b);
 }
 function subtract(a, b) {
-  return a - b;
+  if (a.length === 0 || b.length === 0) {
+    return;
+  }
+  return parseInt(a) - parseInt(b);
 }
 function multiply(a, b) {
-  return a * b;
+  if (a.length === 0 || b.length === 0) {
+    return;
+  }
+  return parseInt(a) * parseInt(b);
 }
 function divide(a, b) {
-  if (b !== null) {
-    return a / b;
+  if (a.length === 0 || b.length === 0) {
+    return;
+  } else if (b == 0) {
+    return "Div by 0";
   }
-  return "Cannot divide by zero";
+  return parseInt(a) / parseInt(b);
 }
 
 function operate(f, a, b) {
-  return f(a, b);
+  return window[f](a, b);
 }
-
-function updateResult(e) {
-  const result = document.querySelector(".result");
-  result.textContent += e.target.innerText;
+function getDisplayValue() {
+  let result = document.querySelector(".result");
+  return result.textContent;
+}
+function updateDisplay(displayValue) {
+  let result = document.querySelector(".result");
+  result.textContent = displayValue;
+}
+function updateDisplayedResult(e) {
+  //to avoid overflowing the window
+  if (getDisplayValue().length == 32) {
+    return;
+  }
+  let displayValue = getDisplayValue() + e.target.innerText;
+  updateDisplay(displayValue);
+  if (e.target.classList[0] == "operator") {
+    clearDisplay();
+  }
 }
 
 function clearDisplay() {
-  const result = document.querySelector(".result");
-  result.textContent = "";
+  updateDisplay("");
 }
 
-const numberKeys = Array.from(document.querySelectorAll(".num"));
-const clearKey = document.getElementById("clear");
-clearKey.addEventListener("click", clearDisplay);
+function saveOperation(e) {
+  let previousNumber = getDisplayValue();
+  let operator = e.target.dataset.operator;
+  previousOperationValues[0] = previousNumber;
+  previousOperationValues[1] = operator;
+}
 
-numberKeys.forEach((key) => key.addEventListener("click", updateResult));
+function calculateResult() {
+  let value = operate(
+    previousOperationValues[1],
+    previousOperationValues[0],
+    getDisplayValue()
+  );
+  previousOperationValues = [];
+  return value;
+}
+function displayResult() {
+  updateDisplay(calculateResult());
+}
+let previousOperationValues = [];
+
+const numberKeys = Array.from(document.querySelectorAll(".num"));
+const operatorKeys = Array.from(document.querySelectorAll(".operator"));
+const clearKey = document.getElementById("clear");
+const equalKey = document.getElementById("equal");
+
+numberKeys.forEach((key) =>
+  key.addEventListener("click", updateDisplayedResult)
+);
+operatorKeys.forEach((key) => key.addEventListener("click", saveOperation));
+operatorKeys.forEach((key) =>
+  key.addEventListener("click", updateDisplayedResult)
+);
+
+clearKey.addEventListener("click", clearDisplay);
+equalKey.addEventListener("click", displayResult);
